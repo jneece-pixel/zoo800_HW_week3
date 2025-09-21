6111fdfe9fa9277b951ca32ef64086ef2771d901
Jillian Neece <jneece@wisc.edu>
2025-09-21 12:45:20 -0500

    Inital commit to repo with the new Rproj. Includes responses to Part 1

---
+++ Coding1_Neece.R
@@ -0,0 +1,15 @@
+## Author: Jillian Neece
+## Zoo800 Week 3 HW: Coding1
+
+## Submitted as an individual assignment because I 
+## was gone for field work during class
+
+## Part 1: Basic Syntax and Variables
+temp_C <- 18.5
+temp_F <- (temp_C* 9/5)+32
+paste("The water temperature is ", 
+      temp_C, 
+      " °C (",
+      temp_F, 
+      " °F)", 
+      sep= "")
